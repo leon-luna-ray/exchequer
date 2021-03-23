b2794833c2a7482cc9abdeccbc77ea73ad75aae8
Ray Luna <leon.luna.ray@gmail.com>
2021-03-22 22:44:29 -0700

    update comments

--- public/db.js
+++ public/db.js
@@ -4,64 +4,54 @@ let db;
 const request = indexedDB.open('budget-tracker', 1);
 
 request.onupgradeneeded = (event) => {
-    const db = event.target.result;
-    db.createObjectStore('pendingtransations', { autoIncrement: true });
+  const db = event.target.result;
+  db.createObjectStore('pendingtransations', { autoIncrement: true });
 };
 
 request.onsuccess = (event) => {
-    db = event.target.result;
-  
-    // check if app is online before reading from db
-    if (navigator.onLine) {
-      checkDatabase();
-    }
-  };
+  db = event.target.result;
 
-  request.onerror = (event) => {
-    console.log("Woops! " + event.target.errorCode);
-  };
+  // check if navigartor is online will run the function to check the database
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
 
-  function saveRecord(record) {
-    // create a transaction on the pending db with readwrite access
-    const transaction = db.transaction(["pendingtransations"], "readwrite");
-  
-    // access your pending object store
-    const budgetStore = transaction.objectStore("pendingtransations");
-  
-    // add record to your store with add method.
-    budgetStore.add(record);
-  };
+request.onerror = (event) => {
+  console.log(`❌ Error ${event.target.errorCode}`);
+};
 
-  function checkDatabase() {
-    // open a transaction on your pending db
-    const transaction = db.transaction(["pendingtransations"], "readwrite");
-    // access your pending object store
-    const budgetStore = transaction.objectStore("pendingtransations");
-    // get all records from store and set to a variable
-    const getAll = budgetStore.getAll();
-  
-    getAll.onsuccess = function() {
-      if (getAll.result.length > 0) {
-        // bulk api call will send everything in the offline storage and send a post request to save to db once back online
-        fetch("/api/transaction/bulk", {
-          method: "POST",
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: "application/json, text/plain, */*",
-            "Content-Type": "application/json"
-          }
-        })
+// Save record offline to the index db
+function saveRecord(record) {
+  const transaction = db.transaction(['pendingtransations'], 'readwrite');
+  const budgetStore = transaction.objectStore('pendingtransations');
+  budgetStore.add(record);
+};
+
+// Check database will see if there is anything in the index db, create fetch request if there is anything in the index db and clear it.
+function checkDatabase() {
+  const transaction = db.transaction(['pendingtransations'], 'readwrite');
+  const budgetStore = transaction.objectStore('pendingtransations');
+  const getAll = budgetStore.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
         .then(response => response.json())
         .then(() => {
-          // if successful, open a transaction on your pending db
-          const transaction = db.transaction(["pendingtransations"], "readwrite");
-  
-          // access your pending object store
-          const budgetStore = transaction.objectStore("pendingtransations");
-  
-          // clear all items in your store
+          const transaction = db.transaction(['pendingtransations'], 'readwrite');
+
+          const budgetStore = transaction.objectStore('pendingtransations');
+          // clear the index db
           budgetStore.clear();
         });
-      }
     };
-  }
+  };
+};
