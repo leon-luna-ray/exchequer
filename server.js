902af446a884bba5dd0e58e60598e974ecddd78a
Ray Luna <leon.luna.ray@gmail.com>
2021-03-27 14:29:04 -0700

    working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // added unified typlology and use create index
-mongoose.connect('mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
