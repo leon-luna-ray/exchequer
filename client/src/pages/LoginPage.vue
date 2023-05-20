22047340e60be102ba89e0de32f3363df15dc918
Ray Luna <leon.luna.ray@gmail.com>
2023-05-20 13:57:05 -0700

    install google auth client

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -1,5 +1,41 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const loginButton = ref(null);
+
+onMounted(() => {
+    if (
+        typeof window === "undefined" ||
+        !window.google ||
+        !loginButton.value
+    ) {
+        return;
+    }
+
+    try {
+        window.google.accounts.id.initialize({
+            ux_mode: "popup",
+            client_id: import.meta.env.VITE_APP_GOOGLE_CLIENT_ID,
+            callback: async (res) => {
+                if (res.credential) {
+                    login(res);
+                }
+            },
+        });
+        window.google.accounts.id.renderButton(loginButton.value, {
+            theme: "filled_blue",
+            size: "medium",
+            type: "standard",
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+</script>
+
 <template>
     <div class="login-panel">
         <h1>Login</h1>
+        <div id="login-button" ref="loginButton"></div>
     </div>
 </template>
