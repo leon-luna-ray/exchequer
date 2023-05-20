9538d2b70cf5ec17778a8311e45f8224ea7842cc
Ray Luna <leon.luna.ray@gmail.com>
2023-05-20 09:22:50 -0700

    base server setup

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// const express = require('express');
-// const logger = require('morgan');
-// const mongoose = require('mongoose');
-// const compression = require('compression');
-import express from 'express';
+gimport express from 'express';
 import mongoose from 'mongoose';
+import compression from 'compression';
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,12 +15,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // added unified typlology and use create index
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
 
 // routes
 // app.use(require('./routes/api.js'));
