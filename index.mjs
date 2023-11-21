5794fa4b6f9227493bd24fff5427e87e7e42eff6
Ray Luna <leon.luna.ray@gmail.com>
2023-11-20 20:49:09 -0800

    rename client dist path

--- index.mjs
+++ index.mjs
@@ -41,7 +41,7 @@ app.use('/api/v1/posts', posts);
 // Static
 app.use(express.static('./client/dist'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 connectDB();
