7d5b679b0735ea43cd50c2556429be650f66631d
Ray Luna <leon.luna.ray@gmail.com>
2023-11-22 22:56:49 -0800

    restructure css files, import fonts

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <header>
-    <router-link to="/">
-      <h1>Exchequer</h1>
-    </router-link>
-    <nav>
-      <router-link to="/dashboard">Dashboard</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/signup">Singup</router-link>
-    </nav>
+    <div class="container">
+
+      <router-link to="/">
+        <h1>Exchequer</h1>
+      </router-link>
+      <nav>
+        <router-link to="/dashboard">Dashboard</router-link>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/signup">Singup</router-link>
+      </nav>
+    </div>
   </header>
 </template>
+<style scoped>
+@import '@/assets/styles/components/Header.css';
+</style>
